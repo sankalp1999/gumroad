e8f52ca4f56ca71755ee70ff22bbb74afd8494e3
sankalp1999 <45618047+sankalp1999@users.noreply.github.com>
2025-06-26 13:32:13 +0530

    Remove unused recurrence_indicator method from RecurringService::Recurrence

--- app/modules/recurring_service/recurrence.rb
+++ app/modules/recurring_service/recurrence.rb
@@ -1,13 +1,6 @@
 # frozen_string_literal: true
 
 module RecurringService::Recurrence
-  def recurrence_indicator
-    {
-      monthly: " a month",
-      yearly: " a year"
-    }[recurrence.to_sym]
-  end
-
   def recurrence_duration
     {
       monthly: 1.month,
